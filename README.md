593d93389b59adfa76875c0ded2b37290029a976
smahlaba023 <smahlaba023@student.wethinkcode.co.za>
2024-06-06 11:11:50 +0200

    Added methods

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Basic-algebraic-calculator
-A basic algebraic calculator.
+    A basic algebraic calculator.
+
+# CONCEPTS:
+    1. Polynomials.
+    2. Factorization.
+    3. Some code from Basic-arithmetic-calculator was used.
+
+# Aim for the creation of this calculator:
+    1. The aim for this calculator is to marry coding and programming with mathematics and advance in both.
+    2. To improve problem solving logic through maths and code.
+
+# Author
+    Siyathemba Thabo Mahlaba
+
--- calculator/src/main/java/basic/algebraic/calculator/Main.java
+++ calculator/src/main/java/basic/algebraic/calculator/Main.java
@@ -1,5 +1,28 @@
 package basic.algebraic.calculator;
+import java.util.Scanner;
 
 public class Main {
-    // TODO
+    public static final Scanner userInput = new Scanner(System.in);
+
+    // Welcome method.
+    public static void welcome() {
+        System.out.println("This is a basic algebraic calculator that explores the concepts of polynomials and factorization.");
+    }
+
+    public static boolean isNumber(String userInput) {
+
+        // try/catch block to try change a string input by the user to an int type.
+        // and catch a NumberFormatException if it cannot become an int.
+        try {
+            Integer.parseInt(userInput);
+            return true;
+        } catch (NumberFormatException e) {
+            System.out.print("Invalid input. ");
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        welcome();
+    }
 }
